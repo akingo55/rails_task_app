cd170c4a75234aaada9ebb4405dcf0c53f8576a9
akingo55 <am1157154@gmail.com>
2021-07-11 22:19:47 +0900

    add helper for validation

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,9 @@
 class Task < ApplicationRecord
   validates :name, presence: true, length: {maximum: 30}
+  validate :validate_name_not_including_comma
+
+  private
+  def validate_name_not_including_comma
+    errors.add(:name, 'cannot include comma in name') if name&.include?(',')
+  end
 end
