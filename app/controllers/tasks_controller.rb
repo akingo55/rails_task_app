34726f0c7131cde5880c499dcc030cfd9bd76f41
akingo55 <am1157154@gmail.com>
2021-07-11 21:56:47 +0900

    add validation

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,9 +12,12 @@ class TasksController < ApplicationController
   end
 
   def create
-    task = Task.new(task_params)
-    task.save!
-    redirect_to tasks_url, notice: "Completed to register task [#{task.name}]."
+    @task = Task.new(task_params)
+    if @task.save!
+      redirect_to tasks_url, notice: "Completed to register task [#{task.name}]."
+    else
+      render :new
+    end
   end
 
   def edit
